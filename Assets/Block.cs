b4d993f9b4575b496d083a568bc46a07cac9ce88
Eoghan <gallageo@tcd.ie>
2018-10-05 14:41:57 +0100

    Added rune Colliders

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block : MonoBehaviour 
 {
 
+	public Text textBox;
 	void OnMouseDown()
     {
         // load a new scene
         Debug.Log( "THis is  " + gameObject.name );
+		textBox.text = gameObject.name;
     }
 
 }
