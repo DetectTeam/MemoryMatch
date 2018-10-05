0ea411370d60b46721a87a87fa208c88eff6d5e9
Eoghan <gallageo@tcd.ie>
2018-10-05 14:24:11 +0100

    Added viking Rune sprites for project

---
+++ Assets/Block.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour 
+{
+
+	void OnMouseDown()
+    {
+        // load a new scene
+        Debug.Log( "THis is  " + gameObject.name );
+    }
+
+}
